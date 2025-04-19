cfd6b640a49fc3ff2674d94a2e3017c6cb54dca3
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-04-19 21:42:38 +0530

    Time: 1 ms (82.82%) | Memory: 43.8 MB (67.96%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        int n=nums.length;
-        int count=0;
-        int arr[]=new int[n];
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]){
-               nums[i]=2*nums[i];
+               nums[i]= nums[i]*2;
                nums[i+1]=0;
             }
         }
+        int idx=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                arr[count++]=nums[i];
-            }
+              if(nums[i]!=0){
+                int temp=nums[idx];
+                nums[idx]=nums[i];
+                nums[i]=temp;
+                idx++;
+              }
         }
-        return arr;
+        return nums;
     }
 }
