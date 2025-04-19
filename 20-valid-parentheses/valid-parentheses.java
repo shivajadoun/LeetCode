4dd09f63c1e34b97bb476ff6bf849dcd7374cb77
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-04-19 09:35:31 +0530

    Time: 2 ms (97.44%) | Memory: 42.2 MB (8.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c); // Push opening brackets
+            } else {
+                // Check if the stack is empty or if the brackets don't match
+                if (stack.isEmpty() || 
+                   (c == ')' && stack.pop() != '(') || 
+                   (c == '}' && stack.pop() != '{') || 
+                   (c == ']' && stack.pop() != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty(); // Ensure no unmatched opening brackets remain
+    }
+}
