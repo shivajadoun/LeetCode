1a774f94049581b37a575c3f66e3c4bde146b31a
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-06-01 09:45:02 +0530

    Time: 1 ms (89.71%) | Memory: 41.3 MB (14.71%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public long distributeCandies(int n, int limit) {
+          return totalWays(n) 
+             - 3 * totalWays(n - (limit + 1))
+             + 3 * totalWays(n - 2 * (limit + 1))
+             - totalWays(n - 3 * (limit + 1));
+    }
+    private long totalWays(long sum) {
+        if (sum < 0) return 0;
+        return (sum + 2) * (sum + 1) / 2;
+    }
+}
