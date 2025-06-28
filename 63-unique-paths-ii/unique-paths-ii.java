1dd0859cdb1217ced13ef9a2985671cd01abbdba
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-06-28 12:51:04 +0530

    Time: 1 ms (17.01%) | Memory: 41.7 MB (80.72%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,36 +1,32 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
-        int n=arr.length;
-        int m=arr[0].length;
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(arr[i][j]==1)arr[i][j]=-1;
-            }
+    int m=arr.length;
+    int n=arr[0].length;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==1)arr[i][j]=-1;
         }
-
-        int dp[][]=new int[n][m];
+    }
+    int dp[][]=new int [m][n];
+       for(int i=0;i<m;i++){
+        if(arr[i][0]==-1)break;
+        else dp[i][0]=1;
+       }
         for(int i=0;i<n;i++){
-            if(arr[i][0]==-1)break;
-            dp[i][0]=1;
-        }
-        
-        for(int i=0;i<m;i++){
-            if(arr[0][i]==-1)break;
-            dp[0][i]=1;
-        }
-
-        System.out.println(Arrays.deepToString(dp));
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(arr[i][j]!=-1){
-                    int sum=0;
-                    if(arr[i-1][j]!=-1)  sum+=dp[i-1][j];
-                    if(arr[i][j-1]!=-1)  sum+=dp[i][j-1];
-                    dp[i][j]=sum;
-                }
+        if(arr[0][i]==-1)break;
+        else dp[0][i]=1;
+       }
+       for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(arr[i][j]!=-1){
+                int sum=0;
+                if(arr[i][j-1]!=-1)   sum+=dp[i][j-1];
+                 if(arr[i-1][j]!=-1)   sum+=dp[i-1][j];
+                 dp[i][j]=sum;
             }
         }
-        return dp[n-1][m-1];
+       }
+       return dp[m-1][n-1];
+       
    }
 }
