90d91e2ea67da264941bd11181118b3687aa91ec
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-07-11 09:35:37 +0530

    Time: 44 ms (16.62%) | Memory: 46 MB (5.27%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,29 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] a) {
-        if(a.length==0)return null;
-        List<Integer>ll=new ArrayList<>();
-        for(int i=0;i<a.length;i++){
-            while(a[i]!=null){
-                ll.add(a[i].val);
-                a[i]=a[i].next;
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < lists.length; i++){
+            ListNode curr = lists[i];
+            while(curr != null){
+                System.out.print(curr.val+" ");
+                pq.add(curr.val);
+                curr = curr.next;
             }
-           
         }
-         Collections.sort(ll);
-            ListNode head=null;
-            ListNode temp=null;
-            for(int i=0;i<ll.size();i++){
-                ListNode newNode=new ListNode(ll.get(i));
-                if(head==null){
-                    head=newNode;
-                    temp=head;
-                }else{
-                    temp.next=newNode;
-                    temp=temp.next;
-                }
-            }
-            return head;
+    
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (!pq.isEmpty()) {
+           tail.next = new ListNode(pq.poll());
+           tail = tail.next;
+        }
+        return dummy.next; 
     }
 }
