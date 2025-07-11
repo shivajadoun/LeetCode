0de41e61d3650f82898031db56c49de51dab4bf1
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-07-11 09:28:11 +0530

    Time: 13 ms (24.09%) | Memory: 45.9 MB (13.37%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,32 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-       if (numRows == 1 || numRows >= s.length()) {
-            return s;
+        List<List<Character>> mat = new ArrayList<>();
+        for(int r = 0;r<numRows;r++)
+        {
+            mat.add(new ArrayList<>());
         }
-
-        int idx = 0, d = 1;
-        List<Character>[] rows = new ArrayList[numRows];
-        for (int i = 0; i < numRows; i++) {
-            rows[i] = new ArrayList<>();
-        }
-
-        for (char c : s.toCharArray()) {
-            rows[idx].add(c);
-            if (idx == 0) {
-                d = 1;
-            } else if (idx == numRows - 1) {
-                d = -1;
+        int n = s.length();
+        int i = 0;
+        while(i<n)
+        {
+            for(int down = 0;down<numRows && i<n;down++)
+            {
+                mat.get(down).add(s.charAt(i++));
             }
-            idx += d;
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (List<Character> row : rows) {
-            for (char c : row) {
-                result.append(c);
+            for(int up = numRows-2;up>0  && i<n;up--)
+            {
+                mat.get(up).add(s.charAt(i++));
             }
         }
-
-        return result.toString();        
+        String ans = "";
+        for(i=0;i<numRows;i++)
+        {
+            for(int j=0;j<mat.get(i).size();j++)
+            ans += mat.get(i).get(j);
+        }
+        return ans;
     }
 }
