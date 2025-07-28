800f6bf77d2bd67039dc5bcee2f341cf2b06c45e
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-07-28 19:04:11 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (64.34%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,52 +1,24 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        
-        // Constants for 32-bit signed integer range
-        final int INT_MAX = Integer.MAX_VALUE;
-        final int INT_MIN = Integer.MIN_VALUE;
-        
-        int i = 0;
-        int n = s.length();
-        
-        // Step 1: Skip leading whitespace
-        while (i < n && s.charAt(i) == ' ') {
+        s=s.trim();
+        if(s.length()==0)return 0;
+         int i=0;
+         int sign=1;
+         if(s.charAt(i)=='-' ||s.charAt(i)=='+'){
+            sign =s.charAt(i)=='-'?-1:1;
             i++;
-        }
-        
-        // Check if we've reached the end
-        if (i == n) {
-            return 0;
-        }
-        
-        // Step 2: Check for sign
-        int sign = 1;
-        if (s.charAt(i) == '+') {
-            i++;
-        } else if (s.charAt(i) == '-') {
-            sign = -1;
-            i++;
-        }
-        
-        // Step 3: Read digits and convert
-        long res = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            int digit = s.charAt(i) - '0';
-            res = res * 10 + digit;
+         }
+         
+         long res=0;
+         while(i<s.length()&&(s.charAt(i)-'0'>=0 && s.charAt(i)-'0'<=9)){
             
-            if (sign * res <= INT_MIN) {
-                return INT_MIN;
-            }
-            if (sign * res >= INT_MAX) {
-                return INT_MAX;
-            }
-            
-            i++;
-        }
-        
-        // Step 4: Apply sign and return
-        return (int)(res * sign);        
+                // System.out.println(res);
+                res=res*10+(s.charAt(i)-'0');
+                if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE){
+                    return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                }
+                i++;
+         }
+         return (int)res*sign;
     }
 }
