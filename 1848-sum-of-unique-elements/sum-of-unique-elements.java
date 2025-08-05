930bdce80e75f70eaa711b7306e692139c9f7840
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2025-08-05 09:31:46 +0530

    Time: 1 ms (74.69%) | Memory: 44.6 MB (8.40%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+      int arr[]=new int[10001];
+      for(int i=0;i<nums.length;i++){
+        arr[nums[i]]++;
+      }  
+      int ans=0;
+         for(int i=0;i<nums.length;i++){
+              if(arr[nums[i]]==1)ans+=nums[i];
+      }  
+      return ans;
+
+    }
+}
