1786395fa72049525f6007dbdce592eafc1d0d74
SHIVA JADOUN <157496390+shivajadoun@users.noreply.github.com>
2026-02-16 00:17:28 +0530

    Time: 28 ms (7.22%) | Memory: 49.3 MB (5.09%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,19 +1,52 @@
-
 class Solution {
     public String addBinary(String a, String b) {
-          StringBuilder sb=new StringBuilder();
-
-          int i=a.length()-1;
-           int j=b.length()-1;
-          int rem=0;
-         while(i>=0 || j>=0  || rem!=0){
-            int biteA=(i>=0)? a.charAt(i--)-'0':0;
-            int biteB=(j>=0)? b.charAt(j--)-'0':0;
-            int sum=biteA+biteB +rem;
-            sb.append(sum%2);
-            rem=sum/2;
+         int al=a.length()-1;
+         int bl=b.length()-1;
+        String ans="";
+        int c=0;
+         while(al>=0 && bl>=0){
+         int p=(a.charAt(al)-'0')+(b.charAt(bl)-'0')+c;
+         if(p>1)c=1;
+         else c=0;
+         if(p==2 || p==0){
+            ans+='0';
+         }else{
+            ans+='1';
          }
-         return sb.reverse().toString();
+         al--;
+         bl--;
+         System.out.print(c+" ");
+         }
+         if(al>=0){
+            while(al>=0){
+                int p=a.charAt(al)-'0'+c;
+                if(p>1)c=1;
+                else c=0;
+                if(p==2 ||p==0)ans+='0';
+                else ans+='1';
+                al--;
+  System.out.print(c+" ");
+            }
+         }
+            if(bl>=0){
+                while(bl>=0){
+                      int p=b.charAt(bl)-'0'+c;
+                if(p>1)c=1;
+                else c=0;
+                if(p==2 ||p==0)ans+='0';
+                else ans+='1';
+                bl--;
+                  System.out.print(c+" ");
+                }
+            }
+            if(c==1)ans+='1';
+              System.out.print(c+" ");
+            String ans2="";
+            for(int i=ans.length()-1;i>=0;i--){
+                ans2+=ans.charAt(i);
+            }
+            return ans2;
 
+         
     }
 }
